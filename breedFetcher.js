c09399a1a30905cba4e4e01dd75bed57d518e196
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-07 23:29:04 +0000

    began refactoring to modularize code and use callbacks with the async function more approporiately

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,38 @@
 let request = require('request');
 let list = require('./breedId.js');
 
-let requestedSearch = process.argv.slice(2);
+// let requestedSearch = process.argv.slice(2);
 
-let catSearch = requestedSearch.join(' ');
+// let catSearch = requestedSearch.join(' ');
 
-console.log(requestedSearch);
-if (catSearch === '') {
-  throw 'Uh oh, no cat specified in command line';
-}
+// console.log(requestedSearch);
+// if (catSearch === '') {
+  // throw 'Uh oh, no cat specified in command line';
+// }
 
-const breedNames = Object.keys(list.breeds);
-if (breedNames.includes(catSearch) === false) {
-  throw "Oh dear, I don't know that kind of cat, but here's a list of what I do know: " , breedNames;
-}
+// const breedNames = Object.keys(list.breeds);
+// if (breedNames.includes(catSearch) === false) {
+  // throw "Oh dear, I don't know that kind of cat, but here's a list of what I do know: " , breedNames;
+// }
 
-let key = list.breeds[catSearch];
+// let key = list.breeds[catSearch];
 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${key}`, function(error, response, body) {
-  if (error) console.log('error:', error);
-  if (response.statusCode !== 200) console.log('statusCode: ', response && response.statusCode);
 
-  const data = JSON.parse(body);
+const fetchBreedDescription = function(breedName, dataReturner) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    if (error) console.log('error:', error);
+    if (response.statusCode !== 200) console.log('statusCode: ', response && response.statusCode);
+
+    const data = JSON.parse(body);
+    if (!error) dataReturner(data);
+  });
+
+};
+
+const printOutDetails = data => {
   console.log(data[0].description);
-});
+};
 
-
+fetchBreedDescription('Bomb', printOutDetails);
