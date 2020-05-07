89569c08fde687d4dec29ede021fe904973c6e5c
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-07 22:00:51 +0000

    refactor to handle multi word cat names and throw errors if invalid name or no name passed

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,18 +3,27 @@ let list = require('./breedId.js');
 
 let requestedSearch = process.argv.slice(2);
 
+let catSearch = requestedSearch.join(' ');
 
-let key = list.breeds[requestedSearch[0]];
-console.log(key);
+console.log(requestedSearch);
+if (catSearch === '') {
+  throw 'Uh oh, no cat specified in command line';
+}
+
+const breedNames = Object.keys(list.breeds);
+if (breedNames.includes(catSearch) === false) {
+  throw "Oh dear, I don't know that kind of cat";
+}
+
+let key = list.breeds[catSearch];
+
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${key}`, function (error, response, body) {
-  console.log('error:', error);
-  console.log('statusCode: ', response && response.statusCode);
-//  console.log('body: ', body);
+  if (error) console.log('error:', error);
+  if (response.statusCode !== 200) console.log('statusCode: ', response && response.statusCode);
 
   const data = JSON.parse(body);
   console.log(data[0].description);
-
 });
 
 
