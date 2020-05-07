cbc49c348428f0145a9ea1d989de42715b13223c
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-07 21:28:36 +0000

    rather than download a valid registry of breeds we can search for every time we want to find one I created an object storing breed names and the respective id that accompanies them to use

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,14 @@
 let request = require('request');
+let breeds = require('./breedId.js');
 
-// request('https://api.thecatapi.com/v1/breeds/search?q=sib', function (error, response, body) {
-  // console.log('error:', error);
-  // console.log('statusCode: ', response && response.statusCode);
-// //  console.log('body: ', body);
-
-  // const data = JSON.parse(body);
-  // console.log(data[0].description);
-
-// });
-
-// console.log(typeof body);
-
-
-
-request('https://api.thecatapi.com/v1/breeds', function (error, response, body) {
+request('https://api.thecatapi.com/v1/breeds/search?q=sib', function (error, response, body) {
   console.log('error:', error);
   console.log('statusCode: ', response && response.statusCode);
-  //console.log('body: ', body);
+//  console.log('body: ', body);
 
   const data = JSON.parse(body);
-  data.forEach(cat => {
-    console.log(`name: ${cat.name}  ID: ${cat.id}`);
-  });
+  console.log(data[0].description);
 
 });
 
-console.log(typeof body);
+
