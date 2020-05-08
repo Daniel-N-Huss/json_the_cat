2c42652c56dd1cddf75b4ce0792afdfd2b2dc23e
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-08 00:25:33 +0000

    discard breedID to follow assignment spec after it was refactored out _ search function works propery and an error is thrown if the api returns a null value but does not throw an error on its own

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,23 @@
 let request = require('request');
-let list = require('./breedId.js');
-
-
-// const breedNames = Object.keys(list.breeds);
-// if (breedNames.includes(catSearch) === false) {
-  // throw "Oh dear, I don't know that kind of cat, but here's a list of what I do know: " , breedNames;
-// }
-
-// let key = list.breeds[catSearch];
-
-
 
 const fetchBreedDescription = function(breedName, dataReturner) {
 
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    if (error) {
+      dataReturner(error, null);
+    }
+    
+    if (body === '[]') {
+      throw "Sorry about that, I couldn't find that cat breed";
+    }
+
     const retreivedData = JSON.parse(body);
     const descriptionData = retreivedData[0].description;
-    dataReturner(error, descriptionData);
-  });
 
+    if (!error) {
+      dataReturner(null, descriptionData);
+    }
+  });
 };
 
 module.exports = { fetchBreedDescription };
--- index.js
+++ index.js
@@ -7,7 +7,6 @@ if (breedName === undefined) {
 }
 
 
-
 fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log('Error fetch details:', error);
